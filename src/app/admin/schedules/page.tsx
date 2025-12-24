2fbf7e06893f977700b4e16b6e1701bbd2b5aaa9
tegarw1803 <103875212+tegarw1803@users.noreply.github.com>
2025-12-24 10:11:26 +0700

    Update page.tsx

--- src/app/admin/schedules/page.tsx
+++ src/app/admin/schedules/page.tsx
@@ -1,13 +1,5 @@
 "use client";
 
-import { useState } from 'react'
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-} from "@/components/ui/dialog"
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
@@ -36,7 +28,15 @@ import {
   SidebarMenuItem,
   SidebarHeader,
   SidebarFooter,
-} from '@/components/ui/sidebar'
+}
+  import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog"
+  from '@/components/ui/sidebar'
 import {
   LayoutDashboard,
   Users,
